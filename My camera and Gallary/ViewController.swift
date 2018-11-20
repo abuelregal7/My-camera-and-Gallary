21499385da72e148c722423461c09e44ffc02144
mhr <mhr@mhrs-imac.local>
2018-11-20 12:36:25 -0800

    Initial Commit

---
+++ My camera and Gallary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  My camera and Gallary
+//
+//  Created by mhr on 11/20/18.
+//  Copyright © 2018 mhr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
