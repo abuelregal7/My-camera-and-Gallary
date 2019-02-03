6a3efb3b8836985b9df0f5d3cc7b578439e91f1f
ahmed abu elregal <ahmedabuelregal@192.168.181.130>
2019-02-02 16:54:57 -0800

    7

--- My camera and Gallary/ViewController.swift
+++ My camera and Gallary/ViewController.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var Photos: UIButton!
+    @IBOutlet weak var camera: UIButton!
+    @IBOutlet weak var ImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +24,29 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+@IBAction func PhotoAction (_ sender: UIButton) {
+    
+    let picker = UIImagePickerController()
+    picker.delegate = self
+    picker.sourceType = .photoLibrary
+    present(picker, animated: true, completion: nil)
+    
+    
 }
+    @IBAction func CameraAction  (_ sender: UIButton) {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .camera
+        present(picker, animated: true, completion: nil)
+    }
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        let image = info[UIImagePickerControllerEditedImage] as! UIImage
+        ImageView.image = image
+        
+        [UIImagePickerControllerOriginalImage]as?UIImage;
+        dismiss(animated: true, completion: nil)
+    }
+    }
 
